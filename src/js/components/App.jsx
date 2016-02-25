e5d14328612421eeb6caac7ec6d2c7731a4816f9
ziyadparekh <ziyad.parekh@gmail.com>
2016-02-25 14:07:31 -0800

    Commit to push to github

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -6,10 +6,9 @@ class App extends Component {
     return (
       <Panel className="col-xs-8" style={{marginLeft:"auto", marginRight: "auto", float: "none"}}>
         <form className="form-horizontal">
-          <Input type="text" label="Username" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-          <Input type="password" label="Password" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-          <Input type="checkbox" label="Remember Me" wrapperClassName="col-xs-offset-2 col-xs-10" help="Offset is applied to wrapper." />
-          <ButtonInput type="submit" value="Submit Button" className="pull-right" />
+          <Input label="Username" type="text" />
+          <Input label="Passowrd" type="password" />
+          <ButtonInput type="submit" value="Login" />
         </form>
       </Panel>
     )
